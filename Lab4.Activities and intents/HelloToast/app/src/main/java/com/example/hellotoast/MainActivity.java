21b197bab52d933e8cf5e29e259c6ac9f969a7d2
1811495 <1811495@mail.nankai.edu.cn>
2021-04-07 00:53:03 +0800

    add hello toast.

---
+++ Lab4.Activities and intents/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
